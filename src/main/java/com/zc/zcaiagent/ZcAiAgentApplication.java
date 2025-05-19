72190718c89238c6de9f779a3ce517da316716bc
zcnovice <2739650633@qq.com>
2025-05-19 20:06:13 +0800

    初始化项目

---
+++ src/main/java/com/zc/zcaiagent/ZcAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zc.zcaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcAiAgentApplication.class, args);
+    }
+
+}
