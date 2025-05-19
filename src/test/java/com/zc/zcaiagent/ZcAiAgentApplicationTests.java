cb24e82baaaa37d066e71a7b143f943ae5c63104
zcnovice <2739650633@qq.com>
2025-05-19 23:39:57 +0800

    HTTP接入AI(要通过AI装换成Java代码)-1

---
+++ src/test/java/com/zc/zcaiagent/ZcAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.zcaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
